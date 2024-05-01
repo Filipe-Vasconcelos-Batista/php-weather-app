510e790dafe8b2df4328d204f4819d2259ddda50
Filipe Batista <filipevbatista@live.com>
2024-05-01 07:23:45 +0200

    lets try this

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+    Weather Reporto
+</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container d-flex justify-content-center mt-2">
+<form class="row g-2">
+<div class="col-auto ">
+    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Choose your city">
+</div>
+    <div class="col-auto d">
+        <button type="submit" class="btn btn-primary mb-3">Submit</button>
+    </div>
+
+</form>
+</div>
+<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
