efc09f1f855d0e79784577b6a6c103dd9c056ff0
Filipe Batista <filipevbatista@live.com>
2024-05-02 16:29:23 +0200

    feature:spi connection is pretty much ready

---
+++ classes/weatherApi.php
@@ -0,0 +1,20 @@
+<?php
+class weatherApi {
+    public $apiKey;
+    private $apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+    public function __construct($apiKey) {
+        $this->apiKey = $apiKey;
+    }
+    public function getWeather($city) {
+        $url=$this->apiUrl . urlencode($city) . '&appid=' . $this->apiKey . '&units=metric';
+        $weather=file_get_contents($url);
+        if ($weather === FALSE) {
+            return "City does not exist";
+        }
+        $weatherData=json_decode($weather,true);
+        if ($weatherData['cod'] != 200) {
+            return "Error: " . $weatherData["message"];
+        }
+        return $weatherData;
+    }
+}
